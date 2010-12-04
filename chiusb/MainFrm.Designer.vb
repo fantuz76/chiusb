edf9389e29b74ca0b6d8113b49cc9d8e42ad2351
fantuz76@gmail.com <fantuz76@gmail.com@358dc7b2-69ef-07e9-509d-3cdd6321f382>
2010-12-04 00:27:37 +0000

    minors

--- chiusb/MainFrm.Designer.vb
+++ chiusb/MainFrm.Designer.vb
@@ -24,15 +24,16 @@ Partial Class MainFrm
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.btnExit = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.btnRead = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
@@ -48,7 +49,7 @@ Partial Class MainFrm
         Me.TabControl1.Location = New System.Drawing.Point(5, 72)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(656, 268)
+        Me.TabControl1.Size = New System.Drawing.Size(584, 268)
         Me.TabControl1.TabIndex = 0
         '
         'TabPage1
@@ -57,10 +58,18 @@ Partial Class MainFrm
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(648, 242)
+        Me.TabPage1.Size = New System.Drawing.Size(576, 242)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "TabPage1"
         Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 6)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(349, 230)
+        Me.TextBox1.TabIndex = 2
         '
         'TabPage2
         '
@@ -74,26 +83,18 @@ Partial Class MainFrm
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(667, 301)
+        Me.btnExit.Location = New System.Drawing.Point(694, 301)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(90, 35)
         Me.btnExit.TabIndex = 1
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(6, 6)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(219, 230)
-        Me.TextBox1.TabIndex = 2
-        '
         'StatusStrip1
         '
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 348)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(769, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(794, 22)
         Me.StatusStrip1.TabIndex = 3
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -102,7 +103,7 @@ Partial Class MainFrm
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(769, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(794, 24)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -116,32 +117,41 @@ Partial Class MainFrm
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'GroupBox1
         '
         Me.GroupBox1.Location = New System.Drawing.Point(5, 27)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(690, 39)
+        Me.GroupBox1.Size = New System.Drawing.Size(635, 39)
         Me.GroupBox1.TabIndex = 5
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "GroupBox1"
         '
         'btnRead
         '
-        Me.btnRead.Location = New System.Drawing.Point(667, 92)
+        Me.btnRead.Location = New System.Drawing.Point(595, 250)
         Me.btnRead.Name = "btnRead"
         Me.btnRead.Size = New System.Drawing.Size(90, 35)
         Me.btnRead.TabIndex = 6
         Me.btnRead.Text = "Read"
         Me.btnRead.UseVisualStyleBackColor = True
         '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(646, 27)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(138, 134)
+        Me.ListBox1.TabIndex = 7
+        '
         'MainFrm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(769, 370)
+        Me.ClientSize = New System.Drawing.Size(794, 370)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.btnRead)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.GroupBox1)
@@ -171,5 +181,6 @@ Partial Class MainFrm
     Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents btnRead As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 
 End Class
